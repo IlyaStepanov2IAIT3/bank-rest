23b31b22f36aefc2c99f76566021a478b11b1c00
Илья Степанов <ilyastepanovwork0@gmail.com>
2025-10-23 19:02:11 +0400

    feat(api): add Bearer JWT security scheme to OpenAPI config

--- src/main/java/com/example/bankcards/config/OpenApiConfig.java
+++ src/main/java/com/example/bankcards/config/OpenApiConfig.java
@@ -1,11 +1,21 @@
 package com.example.bankcards.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfig {
 
     @Bean
@@ -13,6 +23,7 @@ public class OpenApiConfig {
         return new OpenAPI().info(new Info()
                 .title("Bank REST API")
                 .version("1.0")
-                .description("Документация для тестового задания Bank REST"));
+                .description("Документация для тестового задания Bank REST"))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("BearerAuth"));
     }
 }
